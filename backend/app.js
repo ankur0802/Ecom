56a8519120a3eae4efbd4f19eb1319c58d9fb7e8
Ankur singh <ankur@rudrainnovative.in>
2023-04-20 12:58:07 +0530

    updated

--- backend/app.js
+++ backend/app.js
@@ -23,10 +23,10 @@ app.use(fileUpload())
 
 // static files 
 
-app.use(express.static(path.join(__dirname, './frontend/build')))
+app.use(express.static(path.join(__dirname, '../frontend/build')))
 
 app.get('*', (req, res)=>{
-    res.sendFile(path.join(__dirname, './frontend/build/index.html'))
+    res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
 
 })
 
